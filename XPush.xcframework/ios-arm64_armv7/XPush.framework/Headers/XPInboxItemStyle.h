49af7cf0b31b9c252e456a650ee5476b19739464
Karen Walsh <karen.walsh@xtremepush.com>
2022-11-25 16:02:56 +0000

    Prepared new version 5.4.0 [ci skip]

---
+++ XPush.xcframework/ios-arm64_armv7/XPush.framework/Headers/XPInboxItemStyle.h
@@ -0,0 +1,20 @@
+//
+//  XPInboxItemStyle.h
+//  XPush
+//
+//  Created by Vlad Soroka on 10/25/18.
+//  Copyright © 2018 XPush. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XPInboxItemStyle : NSObject
+
+@property (nonatomic, readonly) NSString* background;
+@property (nonatomic, readonly) NSString* titleBackground;
+
+@end
+
+NS_ASSUME_NONNULL_END
